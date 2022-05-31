88f30a90ce3048c85a8cb116863da4129ebacf31
LaiE89 <laiethan2003@gmail.com>
2022-05-31 17:19:17 -0400

    Added new files

---
+++ MyMangaListFiles/src/simulator/ExecMain.java
@@ -0,0 +1,8 @@
+package simulator;
+import java.io.IOException;
+
+public class ExecMain {
+	public static void main(String[] args) throws IOException {
+        simulator.Main.main(args);
+    }
+}
